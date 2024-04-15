1ec8f4ac92ba7acb61b22e1557908b665136d520
Satyajeet Das Chowdhury <satyajeetdc@gmail.com>
2024-04-15 09:21:52 +0530

    added files

---
+++ init/index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "661a71e46efee7928e6dad80",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("Data was initialized");
+};
+
+initDB();
